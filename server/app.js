2665f8ed5918743a814cadf7d6931b2598d30041
Salehen Rahman <salehen.rahman@gmail.com>
2017-08-04 16:36:53 -0700

    Done with the server

--- server/app.js
+++ server/app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const crypto = require('crypto');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use(express.static(path.resolve(__dirname, 'public')));
