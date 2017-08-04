4a80199c41c47d34df2b1edf23cda6a04a99e9b5
Matt Lockyer <mattdlockyer@gmail.com>
2017-08-04 14:45:03 -0700

    added studyblock contract

--- web3/contracts/Migrations.sol
+++ web3/contracts/Migrations.sol
@@ -1,5 +1,6 @@
-pragma solidity ^0.4.4;
 
+
+pragma solidity ^0.4.0;
 contract Migrations {
   address public owner;
   uint public last_completed_migration;
--- web3/migrations/2_deploy_contracts.js
+++ web3/migrations/2_deploy_contracts.js
@@ -1,8 +1,7 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+
+
+var StudyBlock = artifacts.require("./StudyBlock.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  deployer.deploy(StudyBlock);
 };
