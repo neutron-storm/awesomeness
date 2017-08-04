a2c3e62ec14d553dff5c728c46b25c43f033c90c
Salehen Rahman <salehen.rahman@gmail.com>
2017-08-04 16:11:39 -0700

    Added the server

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const crypto = require('crypto');
+const path = require('path');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.post('/content', bodyParser.text(), (req, res) => {
+  res.json({ data: crypto.randomBytes(64).toString('base64') });
+});
+
+app.listen(process.env.PORT || 4000, function () {
+  console.log(this.address());
+});
